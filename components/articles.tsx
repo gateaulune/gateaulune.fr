4cc8fe84a7db884670c7af7e7210086420d83177
clochettes <yanissaadaoui@outlook.fr>
2024-01-29 14:20:13 +0100

    Major Update Index.tsx

---
+++ components/articles.tsx
@@ -0,0 +1,6 @@
+export default function articlesPage(){
+    return(
+        <>
+        </>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,33 @@
 import Head from "next/head";
 import pfp from "./assets/pfp.png";
 import Image from "next/image";
+import Header from "../components/header";
+import Link from "next/link";
+import icon from "./assets/pfp.png";
 export default function index(){
   return(
     <div className="p-6 h-dvh bg-gradient-to-r from-violet-500 to-fuchsia-500 ">
       <Head>
         <title>
-          Moon
+          Moon Make Games
         </title>
+        <link rel="icon" href="./assets/icon.png" sizes="any" />
       </Head>
-      <div className="flex align-center flex-row justify-center h-dvh">
-        <div className="align-center justify-start text-center text-white text-4xl font-bold flex-col ">
-        <Image  
-            src={pfp}
-            width={200}
-            height={200}
-            alt="Kurowne's pfp"
-            />
-          <a>Maintenance</a>
-        </div> 
+      <Header/>
+      <div className="flex justify-around mt-52 ">
+        <div  className="text-xl flex flex-col text-center justify-center">
+          <a>Hello, im Moon "Charlotte" !</a>
+          <a>I'm a little Game developper</a>
+          <a>I'm currently working with </a> 
+          <Link href='http://clochettesgames.fr' className="font-bold underline">ClochettesGames</Link>
+        </div>
+        <Image
+          src={pfp}
+          width={250}
+          height={250}
+          alt="Charlotte's pfp"
+        />
+
       </div>
     </div>
   )
